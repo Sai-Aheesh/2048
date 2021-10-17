466dce23a805207210aee43b65c77c631c4f53a5
Aheesh Reddy <aheesh2@gmail.com>
2021-10-17 15:12:01 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("load", () =>{
+    const grid = document.querySelector('.grid')
+    const score = document.getElementById('score-num')
+    const result = document.getElementById('result')
+    const width = 4
+
+})
