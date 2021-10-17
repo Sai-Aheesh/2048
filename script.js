db22d671cbc8ca79092ff390d75052a1de47c752
Aheesh Reddy <aheesh2@gmail.com>
2021-10-17 15:34:57 +0530

    Created a basic layout to work with

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-document.addEventListener("load", () =>{
-    const grid = document.querySelector('.grid')
+console.log("Hello world!")
+
+document.addEventListener("DOMContentLoaded", () =>{
+    const griddisplay = document.querySelector('.grid')
     const score = document.getElementById('score-num')
     const result = document.getElementById('result')
     const width = 4
+    all_squares = []
 
+    function board() {
+        for (let i=0; i<width*width; i++) {
+            square = document.createElement('div')
+            square.innerHTML = 0
+            griddisplay.appendChild(square)
+            all_squares.push(square)
+        }
+    }
+    board()
 })
