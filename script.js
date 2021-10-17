e9c068695fb95c3c8203ee8328a60257a360af0d
Aheesh Reddy <aheesh2@gmail.com>
2021-10-17 16:14:34 +0530

    Created a random num generator which is used to generate two random 2s and a swipe right function which displays the board after swiping each row to the right

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log("Hello world!")
 
 document.addEventListener("DOMContentLoaded", () =>{
-    const griddisplay = document.querySelector('.grid')
+    const grid = document.querySelector('.grid')
     const score = document.getElementById('score-num')
     const result = document.getElementById('result')
     const width = 4
@@ -11,9 +11,45 @@ document.addEventListener("DOMContentLoaded", () =>{
         for (let i=0; i<width*width; i++) {
             square = document.createElement('div')
             square.innerHTML = 0
-            griddisplay.appendChild(square)
+            grid.appendChild(square)
             all_squares.push(square)
         }
+        generate()
+        generate()
     }
     board()
+
+    //generating random numbers
+    function generate() {
+        let randomNum = Math.floor(Math.random() * all_squares.length)
+        if (all_squares[randomNum].innerHTML == 0) {
+            all_squares[randomNum].innerHTML = 2
+        } else generate()
+
+    }
+
+    //Move right
+    function swipeRight() {
+        for (let i =0; i<width*width; i++) {
+            if (i%4 === 0) {  //checking if it belongs to first column
+                row = []
+                for (let j = 0; j<4; j++){
+                    row.push(parseInt(all_squares[i+j].innerHTML))
+                }
+                console.log(row)
+
+                let filterRow = row.filter(num => num)
+                let missing = 4 - filterRow.length
+                let zeroes = Array(missing).fill(0)
+                let newRow = zeroes.concat(filterRow)
+                console.log(newRow)
+
+                for (let j = 0; j < 4; j++) {
+                    all_squares[i+j].innerHTML = newRow[j]
+                }
+            }
+        }
+
+    }
+    swipeRight()
 })
